a33b7a26861996f5a6e62d0be7daa70bea281249
Nicolas Molina Monroy <hi@nicobytes.com>
2023-12-01 16:27:59 -0400

    Fix test

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,10 @@ module.exports = {
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.(ts|tsx)$': 'esbuild-jest',
+    '^.+\\.js$': 'babel-jest',
   },
   moduleNameMapper: {
     '^@src/(.*)$': '<rootDir>/src/$1',
   },
+  transformIgnorePatterns: [ "node_modules/(?!(yaml/browser))" ],
 }
