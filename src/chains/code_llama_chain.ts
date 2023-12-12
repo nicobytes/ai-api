a66fea2e697984559b9e01109c3a7edf7d54f367
Nicolas Molina Monroy <hi@nicobytes.com>
2023-12-12 19:45:10 -0400

    save

--- src/chains/code_llama_chain.ts
+++ src/chains/code_llama_chain.ts
@@ -3,19 +3,19 @@ import { StringOutputParser } from "langchain/schema/output_parser";
 import { RunnableSequence } from "langchain/schema/runnable";
 import { BufferWindowMemory } from "langchain/memory";
 import { ChatCloudflareWorkersAI } from "langchain/chat_models/cloudflare_workersai";
-import { ChatOpenAI } from "langchain/chat_models/openai";
 
 type Params = {
   cloudflareAccountId: string;
   cloudflareApiToken: string;
   baseUrl: string;
-  openAIApiKey: string;
 };
 
-export const buildCodeLlamaChain = async ({ cloudflareAccountId, cloudflareApiToken, baseUrl, openAIApiKey }: Params) => {
-  const model = new ChatOpenAI({
-    modelName: "gpt-4-1106-preview",
-    openAIApiKey
+export const buildCodeLlamaChain = async ({ cloudflareAccountId, cloudflareApiToken, baseUrl }: Params) => {
+  const model = new ChatCloudflareWorkersAI({
+    model: "@hf/thebloke/codellama-7b-instruct-awq",
+    cloudflareAccountId,
+    cloudflareApiToken,
+    baseUrl
   });
 
   const prompt = buildPrompt();
@@ -44,39 +44,7 @@ export const buildCodeLlamaChain = async ({ cloudflareAccountId, cloudflareApiTo
   ]);
 };
 
-const systemPrompt = `Tú eres un asistente que tiene que guiar al estudiante a resolver un ejercicio de código en JavaScript sin darle la solución, debes solo guiarlo.
-
-El ejercicio está dentro de la etiqueta <ejercicio> y el código que el estudiante escribe está dentro de la etiqueta <files>
-
-<ejercicio>
-Tienes un array de números, tu reto es retornar los números de ese array multiplicados por dos.
-
-Para solucionarlo vas a encontrar una función llamada multiplyElements que recibe un parámetro de entrada:
-
-array: Un array de números
-Dentro del cuerpo de la función multiplyElements debes escribir tu solución.
-
-Ejemplo 1:
-
-Input: [2, 4, 5, 6, 8]
-Output: [4, 8, 10, 12, 16]
-
-Ejemplo 2:
-
-Input: [1, 1, -2, -3]
-Output: [2, 2, -4, -6]
-</ejercicio>
-
-
-<files>
-index.js
-\`\`\`js
-export function multiplyElements(array) {{
-  // Tu código aquí 👈
-}}
-\`\`\`
-</files>
-`;
+const systemPrompt = `Tú eres un asistente de python`;
 
 const buildPrompt = () => {
   const prompt = ChatPromptTemplate.fromMessages([
