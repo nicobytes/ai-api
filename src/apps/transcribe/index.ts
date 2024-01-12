48c98aac209312f7766382ff5462021433465a27
Nicolas Molina Monroy <hi@nicobytes.com>
2024-01-12 13:04:38 -0400

    Refactor transcription API endpoint

--- src/apps/transcribe/index.ts
+++ src/apps/transcribe/index.ts
@@ -1,7 +1,6 @@
 import { OpenAPIHono, z } from "@hono/zod-openapi";
 import { Bindings } from "@src/bindings";
 import whisperRoute from "./routes/whisper";
-import { getGatewayUrl } from "@src/utils/getGatewayUrl";
 
 const app = new OpenAPIHono<{ Bindings: Bindings }>();
 
@@ -12,24 +11,28 @@ app.openapi(whisperRoute, async (c) => {
   const blob = await audioFile.arrayBuffer();
   const audio = [...new Uint8Array(blob)];
 
-  const gatewayUrl = getGatewayUrl({
-    endpoint: c.env.CF_AI_GATEWAY_ENDPOINT,
-    account_id: c.env.CF_ACCOUNT_ID,
-    slug: c.env.CF_AI_GATEWAY_SLUG,
-    provider: c.env.CF_AI_GATEWAY_PROVIDER,
-    model: "@cf/openai/whisper",
-  });
-
-  const request = await fetch(gatewayUrl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${c.env.CF_API_TOKEN}`,
-    },
-    body: JSON.stringify({ audio }),
-  });
-  const response = (await request.json()) as { result: { text: string } };
-  return c.json({ transcribe: response.result.text }, 201);
+  const gatewayUrl = `${c.env.AI_GATEWAY_URL}/workers-ai/@cf/openai/whisper`;
+  try {
+    const request = await fetch(gatewayUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${c.env.CF_API_TOKEN}`,
+      },
+      body: JSON.stringify({ audio }),
+    });
+    if (!request.ok) {
+      const detail = await request.text();
+      return c.json({ error: request.statusText, detail }, request.status);
+    }
+    const response = (await request.json()) as { result: { text: string } };
+    return c.json({ transcribe: response.result.text }, 200);
+  } catch (error) {
+    if (error instanceof SyntaxError) {
+      return c.json({ error: "Invalid JSON" }, 500);
+    }
+    return c.json({ error: error }, 500);
+  }
 });
 
 export default app;
